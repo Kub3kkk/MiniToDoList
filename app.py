7b01bcca322ffddbe7df63a431330b8662e17f6f
Kub3kk <kuba44640@gmail.com>
2026-01-11 16:14:04 +0100

    Added modular list

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
+notes = []
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        note = request.form.get('noteadd')
-        return render_template('index.html', note=note)
+        notes.append(request.form.get('noteadd'))
+        return render_template('index.html', note=notes)
     else:
         return 'failiure'
 if __name__ == '__main__':
